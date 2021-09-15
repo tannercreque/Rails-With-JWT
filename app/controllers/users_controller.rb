ecd0e6f60dc341885c8a961a3f98a971121236b3
Tanner <tanner.creque@gmail.com>
2021-09-15 11:04:36 -0600

    user controller progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+    def index
+        @users = User.all
+        render json: @users
+      end
+    
+      def profile
+        render json: @user
+      end
+    
+      def create
+        @user = User.create user_params
+        render json: @user
+      end
+    
 end
