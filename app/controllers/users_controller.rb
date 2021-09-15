a24bbf1835488a787e3cd221461f0d296dbca791
Tanner <tanner.creque@gmail.com>
2021-09-15 11:06:54 -0600

    controller finish, routes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,46 @@
+
 class UsersController < ApplicationController
+
+    skip_before_action :authenticate, only: %i[create login]
+  
     def index
-        @users = User.all
-        render json: @users
+      @users = User.all
+      render json: @users
+    end
+  
+    def profile
+      render json: @user
+    end
+  
+    def create
+      @user = User.create user_params
+      render json: @user
+    end
+  
+    def login
+      @user = User.find_by username: params[:user][:username]
+  
+      if !@user
+        render json: { error: 'Wrong username or password.' }, status: :unauthorized
+      else
+  
+        if !@user.authenticate params[:user][:password]
+          render json: { error: 'Wrong username or password.' }, status: :unauthorized
+        else
+          payload = { user_id: @user.id }
+          secret = 'snitches get stitches'
+  
+          token = JWT.encode payload, secret
+  
+          render json: { token: token, user: @user }, status: :ok
+        end
       end
-    
-      def profile
-        render json: @user
-      end
-    
-      def create
-        @user = User.create user_params
-        render json: @user
-      end
-    
-end
+    end
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
+  
+  end
